2a8290c7054f55236a2d5c50faefa434f88c2ad6
Helio <helio.costa@gmail.com>
2019-01-30 21:15:23 +0000

    postgres

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,5 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-// -------
-import React from 'react'
-import { render } from 'react-dom'
 
-import App from '../components/App'
-
-render(<App />, document.getElementById('react-root'))
+console.log('Hello World from Webpacker')
